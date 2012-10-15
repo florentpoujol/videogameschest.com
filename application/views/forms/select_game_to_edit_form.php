b5aba47edd44c46506931ad85c21ae0bc0ba7e76
Florent POUJOL <florent.poujol@gmail.com>
2012-10-15 10:43:25 +0200

    refactored select_game_to_edit_form

--- application/views/forms/select_game_to_edit_form.php
+++ application/views/forms/select_game_to_edit_form.php
@@ -1,27 +1,25 @@
 		<div id="select_game_to_edit_form">
+			<h1>Select the game to edit</h1>
 
-			<h2>Select the game to edit</h2>
+			<?php
+			if (isset($form))
+				echo get_form_errors($form); ?> 
 
+			<?php echo form_open("admin/editgame"); ?> 
 <?php
-if (isset($form))
-	echo get_form_errors($form);
-
-echo form_open( 'admin/editgame' );
-
 if (IS_ADMIN) // allow to edit any games
-	$raw_games = get_db_rows( 'games' );
+	$raw_games = get_db_rows("profile_id, name", "profiles");
 else
-	$raw_games = get_db_rows( 'games', 'developer_id', USER_ID );
+	$raw_games = get_db_rows("profile_id, name", "profiles", "developer_id = '".USER_ID."'");
 
 $games = array();
 if ($raw_games !== false) {
 	foreach ($raw_games->result() as $game)
-		$games[$game->game_id] = $game->name;
+		$games[$game->profile_id] = $game->name;
 }
-
-echo form_dropdown( 'game_id_select', $games, null, 'id="game_id_select"' );
-echo ' <label for="game_id_select">Select the game</label>';
 ?>
+				<?php echo form_dropdown("game_id_select", $games, null, 'id="game_id_select"'); ?> 
+				<?php echo ' <label for="game_id_select">Select the game</label>'; ?> 
 				<br>
 				<input type="number" min="1" name="game_id_text" id="game_id_text"> <label for="game_id_text">Or write its id</label> <br>
 				<br>
