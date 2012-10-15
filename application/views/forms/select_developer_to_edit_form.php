d4414029ec1f32f646b7c004e299f02148f5e498
Florent POUJOL <florent.poujol@gmail.com>
2012-10-15 10:43:17 +0200

    refactored select_developer_to_edit_form

--- application/views/forms/select_developer_to_edit_form.php
+++ application/views/forms/select_developer_to_edit_form.php
@@ -1,16 +1,15 @@
-<div id="select_developer_to_edit_form">
-	<h2>Select the developer to edit</h2>
-<?php 
-echo form_open( 'admin/editdeveloper' );
+		<div id="select_developer_to_edit_form">
+			<h1>Select the developer to edit</h1>
+		
+			<?php echo form_open("admin/editdeveloper"); ?>
 
-$devs = get_developers();
-echo form_dropdown( 'developer_id_select', $devs, null, 'id="developer_id_select"' );
-echo ' <label for="developer_id_select">Select the developer</label>';
-?>
-		<br>
-		<input type="number" min="1" name="developer_id_text" id="developer_id_text"> <label for="developer_id_text">Or write its id</label> <br>
-		<br>
-		<input type="submit" name="select_developer_to_edit_form_submitted" value="Edit this developer"> <br>
-		<!--<input type="submit" name="edit_own_account_form_submitted" value="Edit my own account">-->
-	</form>
-</div>
+				<?php echo form_dropdown("developer_id_select", get_users_array("dev"), null, 'id="developer_id_select"' ); ?>
+				<?php echo ' <label for="developer_id_select">Select the developer</label>'; ?>
+
+				<br>
+				<input type="number" min="1" name="developer_id_text" id="developer_id_text"> <label for="developer_id_text">Or write its id</label> <br>
+				<br>
+				<input type="submit" name="select_developer_to_edit_form_submitted" value="Edit this developer"> <br>
+				<!--<input type="submit" name="edit_own_account_form_submitted" value="Edit my own account">-->
+			</form>
+		</div>
