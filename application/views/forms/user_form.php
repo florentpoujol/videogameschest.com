e9a0160e69365901ebd2943e27f15a8f06f80655
Florent POUJOL <florent.poujol@gmail.com>
2012-10-15 11:20:21 +0200

    refactored user_form

--- application/views/forms/user_form.php
+++ application/views/forms/user_form.php
@@ -1,34 +1,32 @@
 <?php
-// form to edit an admin account
-
-if( !isset($form) )
+// form to edit a user form
+if (!isset($form))
 	$form = array();
 elseif( is_object($form) )
 	$form = get_object_vars($form);
 
-$form_items = array( 'administrator_id', 'name', 'email', 'password', 'password2' );
+$form_items = array("user_id", "name", "email", "password", "password2");
 
-foreach( $form_items as $item ) {
-	if( !isset($form[$item]) )
-		$form[$item] = '';
+foreach ($form_items as $item) {
+	if( ! isset($form[$item]) )
+		$form[$item] = "";
 }
-
-
-echo '<div id="admin_form">
-';
-
-echo get_form_errors();
-echo get_form_success($form);
-
-echo form_open( 'admin/editadmin' );
-
-echo 'Id : '.$form['administrator_id'].'
-<input type="hidden" name="form[administrator_id]" value="'.$form['administrator_id'].'"><br>';
 ?>
-		<input type="text" name="form[name]" id="name" placeholder="Name" value="<?php echo $form['name'];?>"> <label for="name">Name</label> <br>
-		<input type="email" name="form[email]" id="email" placeholder="Email" value="<?php echo $form['email'];?>"> <label for="email">Email</label> <br>
-		<input type="password" name="form[password]" id="password" placeholder="Write here only to update" value=""> <label for="password">Password</label> <br>
-		<input type="password" name="form[password2]" id="password2" placeholder="Same as above" > <label for="password2">Password confirmation</label> <br>
-		<input type="submit" name="admin_form_submitted" value="Submit">
-	</form>
-</div> <!-- /#admin_form -->
+		<div id="user_form">
+			<h1>Edit your user account</h1>
+
+			<!-- forms errors and success -->
+			<?php echo get_form_errors($form); ?> 
+			<?php echo get_form_success($form); ?> 
+			<!-- /forms errors and success -->
+
+			<?php echo form_open("admin/edituser"); ?>
+				<?php echo '<input type="hidden" name="form[user_id]" value="'.$form['user_id'].'"> Your user Id : '.$form["user_id"]; ?> <br>
+				<input type="text" name="form[name]" id="name" placeholder="Name" value="<?php echo $form['name'];?>"> <label for="name">Name</label> <br>
+				<input type="email" name="form[email]" id="email" placeholder="Email" value="<?php echo $form['email'];?>"> <label for="email">Email</label> <br>
+				<input type="password" name="form[password]" id="password" placeholder="Write here only to update" value=""> <label for="password">Password</label> <br>
+				<input type="password" name="form[password2]" id="password2" placeholder="Same as above" > <label for="password2">Password confirmation</label> <br>
+				<input type="submit" name="user_form_submitted" value="Submit">
+			</form>
+		</div>
+		<!-- /#user_form --> 
