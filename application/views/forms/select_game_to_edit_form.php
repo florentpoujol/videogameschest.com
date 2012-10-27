9f600e9229e2a7416fab3a0ffdf930e4ca0fe462
Florent POUJOL <florent.poujol@gmail.com>
2012-10-27 15:10:46 +0200

    added bootstrap

--- application/views/forms/select_game_to_edit_form.php
+++ application/views/forms/select_game_to_edit_form.php
@@ -1,11 +1,9 @@
 		<div id="select_game_to_edit_form">
-			<h1>Select the game to edit</h1>
+			<?php echo form_open("admin/editgame", array("class"=>"form-horizontal")); ?> 
+				<legend>Select the game to edit</legend>
 
-			<?php
-			if (isset($form))
-				echo get_form_errors($form); ?> 
+				<?php echo get_form_errors(); ?> 
 
-			<?php echo form_open("admin/editgame"); ?> 
 <?php
 if (IS_ADMIN) // allow to edit any games
 	$raw_games = get_db_rows("id, name", "profiles", "type = 'game'");
@@ -18,11 +16,15 @@ if ($raw_games !== false) {
 		$games[$game->id] = $game->name;
 }
 ?>
-				<?php echo form_dropdown("game_id_select", $games, null, 'id="game_id_select"'); ?> 
-				<?php echo ' <label for="game_id_select">Select the game</label>'; ?> 
-				<br>
-				<input type="number" min="1" name="game_id_text" id="game_id_text"> <label for="game_id_text">Or write its id</label> <br>
-				<br>
-				<input type="submit" name="select_game_to_edit_form_submitted" value="Edit this game"> <br>
+				<div class="control-group">
+					<label class="control-label" for="game_id_select">Select the game</label> 
+					<?php echo form_dropdown("game_id_select", $games, null, 'id="game_id_select" class="controls"'); ?> 
+				</div>
+				<div class="control-group">
+					<label class="control-label" for="game_id_text">Or write its id</label>
+					<input type="number" class="controls" min="1" name="game_id_text" id="game_id_text"> <br>
+				</div>
+
+				<input type="submit" name="select_game_to_edit_form_submitted" value="Edit this game">
 			</form>
 		</div> <!-- /#select_game_to_edit_form -->
