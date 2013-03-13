4c13c66803d58ae68e4306355255508e07f7c12d
Florent POUJOL <florent.poujol@gmail.com>
2013-03-13 14:59:15 +0100

    added local config (removed from .gitignore)

---
+++ application/config/local/vgc.php
@@ -0,0 +1,13 @@
+<?php
+return array(
+    
+    'environment' => 'local',
+
+    'smtp' => array(
+        'server' => 'localhost',
+        'server_port' => 0,
+        'username' => '',
+        'password' => '',
+    ),
+
+);
