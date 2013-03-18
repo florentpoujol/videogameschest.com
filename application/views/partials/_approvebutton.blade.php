3cfc33b9916bb202440392e226326274805bd772
Florent POUJOL <florent.poujol@gmail.com>
2013-03-18 15:33:41 +0100

    initial vgc_light commit

--- application/views/partials/_approvebutton.blade.php
+++
@@ -1,6 +0,0 @@
-<form action="admin/review" method="POST">
-    <input type="hidden" name="review" value="{{ $review }}">
-    <input type="hidden" name="profile" value="{{ $profile_type }}">
-    <input type="hidden" name="id" value="{{ $id }}">
-    <input type="submit" value="Approve" class="btn btn-success">
-</form>
