2950c10cf7dc70a682b2339a1d8754847a7889ce
Florent POUJOL <florent.poujol@gmail.com>
2013-03-18 16:34:26 +0100

    updated extended eloquent model

--- application/models/extendedeloquent.php
+++ application/models/extendedeloquent.php
@@ -17,31 +17,6 @@ class ExtendedEloquent extends Eloquent
     }
 
     //----------------------------------------------------------------------------------
-    // MAGIC METHODS
-
-    /**
-     * Handle the dynamic retrieval of attributes and associations.
-     * 
-     *
-     * @param  string  $key
-     * @return mixed
-     */
-    /*public function __get($key)
-    {
-        $value = parent::__get($key);
-
-        if (is_string($value)) return $value;
-        else return $value;
-    }*/
-
-    // NOTE : it's not a good idea to secure data agains XSS with e() or Security::xss_clean() here
-    // because it will mess up with data that does not need to be escaped (especially json strings)
-
-    // actually e() will mess up with json string but not Security::xss_clean()
-    // I better make sure things I put in json strings are escaped instead
-
-
-    //----------------------------------------------------------------------------------
 
     // for Former bundle
     public function __toString()
